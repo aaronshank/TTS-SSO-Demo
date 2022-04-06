b3ce54df9eaa41518f64d0a6564a0ddc0b56ed93
Aaron Shank <shank@ctwo.net>
2022-04-06 14:47:24 -0400

    completed

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tech Talent South Homework
+
+## Single Sign On Demo
+### Complete the following tutorial
+* https://spring.io/guides/tutorials/spring-boot-oauth2/
+### Submission
+* Submit link to github repo for working project
---
+++ src/test/java/com/oauthexample/singlesignon/SinglesignonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauthexample.singlesignon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglesignonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
